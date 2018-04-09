ebd64a20f923a0b7ff3790cc3f7a931ec0165c3c
Xiaodong <1099718640@qq.com>
2018-04-09 23:25:15 +0800

    Update MainActivity.java
    更新注释

--- MainActivity.java
+++ MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sassassaisn.shortpath.Dijkstra2.Edge;
 import com.example.sassassaisn.shortpath.Dijkstra2.Vertex;
@@ -34,42 +35,48 @@ public class MainActivity extends Activity {
         btn_reset = (Button) findViewById(R.id.btn_reset);
         et_point = (EditText) findViewById(R.id.et_point);
         tv_point = (TextView) findViewById(R.id.textView1);
-        btn_add.setOnClickListener(new OnClickListener() {
 
+        //从edit text中获取起始点和终点
+        btn_add.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO 自动生成的方法存根
-                if (et_point.getText() == null
-                        || et_point.getText().toString() == "") {
-                    btn_add.setClickable(false);
+                if (et_point.getText().toString().trim().equals("")) {
+                    Toast.makeText(MainActivity.this,"请先添加起点然后再添加终点",Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                if (i == 9) {
-                    btn_add.setClickable(false);
-                    btn_add.setText("到达点数上限");
+                if(i == 0){
+                    btn_add.setText("请设置终点");
                 }
-                point[i] = et_point.getText().toString();
+                if (i == 1) {
+                    btn_add.setClickable(false);
+                    btn_add.setText("已设置完毕");
+                }
+                point[i] = et_point.getText().toString().trim();
                 tv_point.setText(tv_point.getText().toString() + point[i]
-                        + ",,");
+                        + ",");
                 i++;
-                et_point.setText("");
+                et_point.setText("");  //清空编辑框
             }
         });
-        btn_ok.setOnClickListener(new OnClickListener() {
 
+        //规划最短路径
+        btn_ok.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 for (int j = 1; j < i; j++) {
                     path += dijkstra(point[j - 1], point[j]);
                 }
+                //显示结果
                 tv_point.setText(path);
+                //禁止按钮
+                btn_ok.setClickable(false);
             }
         });
-        btn_reset.setOnClickListener(new OnClickListener() {
 
+        //重置按钮被单击
+        btn_reset.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO 自动生成的方法存根
                 Intent intent = getIntent();
                 finish();
                 startActivity(intent);
@@ -80,40 +87,44 @@ public class MainActivity extends Activity {
 
     public String dijkstra(String p1, String p2) {
         // 构建有向图
-        Vertex v1 = new Vertex("a");//建点
+        // 建点
+        Vertex v1 = new Vertex("a");
         Vertex v2 = new Vertex("b");
         Vertex v3 = new Vertex("c");
         Vertex v4 = new Vertex("d");
         Vertex v5 = new Vertex("e");
 
-        List<Edge> e1l = v1.adj;//建边
+        //建边
+        List<Edge> e1l = v1.adj;
         List<Edge> e2l = v2.adj;
         List<Edge> e3l = v3.adj;
         List<Edge> e4l = v4.adj;
-        List<Edge> e5l=v5.adj;
+        List<Edge> e5l = v5.adj;
 
-        Edge e12 = new Edge(v2, 45);//赋值边
+        //赋值边
+        //a点
+        Edge e12 = new Edge(v2, 45);
         Edge e13 = new Edge(v3, 30);
         Edge e15 = new Edge(v5, 50);
+        e1l.add(e12);
         e1l.add(e13);
         e1l.add(e15);
-        e1l.add(e12);
-
+        //b点
         Edge e24 = new Edge(v4, 60);
         Edge e21 = new Edge(v1, 45);
         e2l.add(e24);
         e2l.add(e21);
-
+        //c点
         Edge e35 = new Edge(v5, 45);
-        e3l.add(e35);
         Edge e31 = new Edge(v1, 30);
+        e3l.add(e35);
         e3l.add(e31);
-
+        //d点
         Edge e45 = new Edge(v5, 55);
-        e4l.add(e45);
         Edge e42 = new Edge(v2, 60);
+        e4l.add(e45);
         e4l.add(e42);
-
+        //e点
         Edge e51=new Edge(v1, 50);
         Edge e53=new Edge(v3, 45);
         Edge e54=new Edge(v4, 55);
@@ -127,7 +138,8 @@ public class MainActivity extends Activity {
         Dijkstra2.vertexMap.put("c", v3);
         Dijkstra2.vertexMap.put("d", v4);
         Dijkstra2.vertexMap.put("e", v5);
-
+        
+        //返回两点的最短路径
         return Dijkstra2.dijkstra(p1, p2);
     }
 
